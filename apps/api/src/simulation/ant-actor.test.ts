e31d0de68d86c8c50181a2b3ea35cb03157d2586
Connor <cjflowers@pm.me>
2025-07-08 13:05:01 +1000

    fixup! add vitest and first unit test

--- apps/api/src/simulation/ant-actor.test.ts
+++ apps/api/src/simulation/ant-actor.test.ts
@@ -5,15 +5,12 @@ import type { Position } from "./domain";
 
 describe("AntActor", () => {
   it("should change position when in FORAGING state", () => {
-    // Arrange
     const initialPosition: Position = { x: 10, y: 10 };
     const actor = new AntActor(initialPosition);
-    const world = new World(100, 100, { x: 50, y: 50 });
+    const world = new World(100, 100);
 
-    // Act
     actor.update(world);
 
-    // Assert
     expect(actor.position).not.toEqual(initialPosition);
   });
 });
