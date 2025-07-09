d767ea457b957940262a4ff4231583aee30bb010
Connor <cjflowers@pm.me>
2025-07-09 14:55:29 +1000

    web: add tailwind

--- packages/web/src/routes/+page.svelte
+++ packages/web/src/routes/+page.svelte
@@ -3,37 +3,3 @@
   <p>A simulation of ant behavior.</p>
   <a href="/world">Start simulation</a>
 </main>
-
-<style>
-  main {
-    font-family: sans-serif;
-    text-align: center;
-    padding: 2em;
-  }
-
-  h1 {
-    font-size: 2.5em;
-    margin-bottom: 0.5em;
-  }
-
-  p {
-    font-size: 1.2em;
-    margin-bottom: 2em;
-  }
-
-  a {
-    font-size: 1.2em;
-    padding: 0.8em 1.5em;
-    border-radius: 8px;
-    border: 1px solid transparent;
-    background-color: #1a1a1a;
-    color: white;
-    cursor: pointer;
-    text-decoration: none;
-    transition: border-color 0.25s;
-  }
-
-  a:hover {
-    border-color: #646cff;
-  }
-</style>
