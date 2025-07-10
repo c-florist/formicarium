46cd1683808c744121f4bc462a7a1cf7e1dc484b
Connor <cjflowers@pm.me>
2025-07-10 20:10:40 +1000

    core-rs: add test wasm fn

--- packages/core-rs/src/lib.rs
+++ packages/core-rs/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
 }
