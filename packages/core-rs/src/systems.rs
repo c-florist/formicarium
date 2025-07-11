bed1f46357eb4af83cd9b2699292899a1e317411
Connor <cjflowers@pm.me>
2025-07-11 16:49:05 +1000

    core-rs: basic wandering system

--- packages/core-rs/src/systems.rs
+++ packages/core-rs/src/systems.rs
@@ -1,4 +1,4 @@
-use crate::components::{Position, Velocity};
+use crate::components::{Position, Velocity, Wandering};
 use hecs::World;
 
 pub fn movement_system(world: &mut World) {
@@ -9,10 +9,18 @@ pub fn movement_system(world: &mut World) {
     }
 }
 
+pub fn wandering_system(world: &mut World) {
+    for (_entity, (vel, _)) in world.query_mut::<(&mut Velocity, &Wandering)>() {
+        // Simple deterministic wandering: slightly alter velocity
+        vel.dx += 0.1;
+        vel.dy -= 0.1;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::components::{Position, Velocity};
+    use crate::components::{Position, Velocity, Wandering};
     use hecs::World;
 
     #[test]
@@ -31,5 +39,20 @@ mod tests {
         let pos = world.get::<&Position>(entity).unwrap();
         assert_eq!(pos.x, 12.5);
         assert_eq!(pos.y, 8.5);
+    }
+
+    #[test]
+    fn test_wandering_system() {
+        // 1. Setup
+        let mut world = World::new();
+        let entity = world.spawn((Velocity { dx: 1.0, dy: 1.0 }, Wandering));
+
+        // 2. Action
+        wandering_system(&mut world);
+
+        // 3. Assertion
+        let vel = world.get::<&Velocity>(entity).unwrap();
+        assert_eq!(vel.dx, 1.1);
+        assert_eq!(vel.dy, 0.9);
     }
 }
