2290bfd8f78272e07ea82070edd7626053c11bfb
Connor <cjflowers@pm.me>
2025-07-16 21:23:01 +1000

    web: remove old world canvas

--- packages/desktop/src/main.rs
+++ packages/desktop/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Import your simulation code
 mod components;
 mod dto;
 mod maths;
