2298ad3f6f62cd95a602c14f584d58b85ee03969
Connor <cjflowers@pm.me>
2025-07-23 21:29:15 +1000

    web: remove redundant layout script

--- packages/web/src/routes/world/+layout.ts
+++
@@ -1,8 +0,0 @@
-import { simulationState } from "$lib/state/simulation.svelte";
-import { redirect } from "@sveltejs/kit";
-
-export const load = () => {
-  if (!simulationState.isRunning) {
-    redirect(307, "/");
-  }
-};
