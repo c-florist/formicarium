497dd5afbaddbcc838c5e33daba9223cf97f66f7
Connor <cjflowers@pm.me>
2025-07-30 21:19:08 +1000

    add icon to navbar heading

--- packages/web/src/lib/components/ui/Navbar.svelte
+++ packages/web/src/lib/components/ui/Navbar.svelte
@@ -7,7 +7,8 @@ let { children }: { children: Snippet } = $props();
 
 <nav class="w-full bg-stone-800 text-white p-4 border-b-4 border-stone-900">
   <div class="flex space-x-4 justify-between items-center">
-    <button onclick={confirmAndExit} class="cursor-pointer">
+    <button onclick={confirmAndExit} class="cursor-pointer flex items-center space-x-2">
+      <img src="/icon/favicon-96x96.png" alt="Formicarium logo" class="h-8 w-8" />
       <h1 class="text-2xl font-bold">Formicarium</h1>
     </button>
     <div class="flex items-center space-x-4">
