ebe73a5eb1e739892cf3428c6eb41ac5c0bb6d71
Connor <cjflowers@pm.me>
2025-08-02 22:12:31 +1000

    web: remove navbar text if on mobile

--- packages/web/src/lib/components/ui/Navbar.svelte
+++ packages/web/src/lib/components/ui/Navbar.svelte
@@ -9,7 +9,7 @@ let { children }: { children: Snippet } = $props();
   <div class="flex space-x-4 justify-between items-center">
     <button onclick={confirmAndExit} class="cursor-pointer flex items-center space-x-2">
       <img src="/icon/favicon-96x96.png" alt="Formicarium logo" class="h-8 w-8" />
-      <h1 class="text-2xl font-bold">Formicarium</h1>
+      <h1 class="text-2xl font-bold hidden sm:block">Formicarium</h1>
     </button>
     <div class="flex items-center space-x-4">
       {#if children}
